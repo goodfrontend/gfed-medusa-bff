7ca54e99f8aaab320f0257fc13d59a24b33c0d5c
Elloani Ross A. Pitogo <pelloani@gmail.com>
2026-04-06 18:26:05 +0800

    feat: trim BROWSE_ATTRIBUTES_TO_RETRIEVE

--- apps/subgraphs/products/src/constants/algolia.ts
+++ apps/subgraphs/products/src/constants/algolia.ts
@@ -9,17 +9,9 @@ export const ATTRIBUTES_TO_RETRIEVE = [
 export const BROWSE_ATTRIBUTES_TO_RETRIEVE = [
   'id',
   'title',
-  'description',
   'handle',
   'thumbnail',
-  'collection_id',
-  'collection_handle',
-  'category_ids',
-  'category_handles',
-  'is_sellable',
   'price_amount',
   'original_price_amount',
   'currency_code',
-  'display_price',
-  'display_original_price',
 ];
